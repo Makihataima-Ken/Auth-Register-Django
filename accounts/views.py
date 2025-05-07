425080aaf2e75acc0be208a0d093dc84e8325d59
Ken <ahmadsoud63@gmail.com>
2025-05-07 15:13:16 +0300

    add profile view

--- accounts/views.py
+++ accounts/views.py
@@ -13,6 +13,8 @@ from django.contrib.auth import get_user_model
 from django.db.models import Q
 import bcrypt
 
+from rest_framework.permissions import IsAuthenticated
+
 class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
     permission_classes = [AllowAny]
@@ -43,3 +45,13 @@ class LoginView(APIView):
             }, status=status.HTTP_200_OK)
 
         return Response({'error': 'Invalid password'}, status=status.HTTP_401_UNAUTHORIZED)
+    
+class ProfileView(APIView):
+    permission_classes = [IsAuthenticated]  # Require JWT toke
+    def get(self, request):
+        user = request.user
+        return Response({
+            "id": user.id,
+            "username": user.username,
+            "email": user.email,
+        })
